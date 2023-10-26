ff05c46c55204796c3e747f1f6adb56616421525
Lech Albrzykowski <lech.albrzykowski@comarch.com>
2023-10-26 11:35:10 +0200

    feat: Adds Command pattern

---
+++ lib/Pattern/Command/AccountingOffice.php
@@ -0,0 +1,50 @@
+<?php
+namespace Pattern\Command;
+/**
+ * AccountingOffice class
+ * @package Patterns
+ * @subpackage Command
+ * @category behavioral
+ */
+class AccountingOffice
+{
+    /**
+     * Array of commands
+     * @var array
+     */
+    private $commands = [];
+    /**
+     * Adds client's commands
+     * @return void
+     */
+    public function addCommand(Executable $command)
+    {
+        $this->commands[] = $command;
+    }
+    /**
+     * Process Tax payment
+     * @return boolean
+     */
+    public function payTax()
+    {
+        foreach ($this->commands as $command) {
+            $executedCommands[] = $command;
+            if ($command->execute() === false) {
+                
+                return sprintf(
+                    '<p>Something went wrong! %s</p>',
+                    $this->undo($executedCommands)
+                );
+            }
+            return '<p>Taxes payed!</p>';
+        }
+    }
+    /**
+     * Undos commands
+     * @return void
+     */
+    public function undo($executedCommands)
+    {
+        // implement
+    }
+}
---
+++ lib/Pattern/Command/Executable.php
@@ -0,0 +1,12 @@
+<?php
+namespace Pattern\Command;
+/**
+ * Executable interface
+ * @package Patterns
+ * @subpackage Command
+ * @category behavioral
+ */
+interface Executable
+{
+    // implement
+}
